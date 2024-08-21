fa8f9ccc67c03dc1f23f81a44173117897d1cf2f
thiaarjo <ticoazevedo742@gmail.com>
2024-08-21 14:48:42 +0000

    Aula2-C

---
+++ PesoAltura.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+typedef struct
+{
+    int Peso;
+    int Altura;
+}pesoaltura;
+
+Int main(){
+PesoAltura Pessoa;
+pessoa.peso=95
+pessoa.altura=180;
+printf("Peso: %i, Altura %i\n",pessoa.peso, pessoa.altura) 0;
+return 0;
+}
